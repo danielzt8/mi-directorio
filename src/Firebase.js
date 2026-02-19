e387ea1283d60cd88dd17619d8f47e62dc2ab64f
Daniel <nikolazt98@gmail.com>
2026-02-19 00:24:55 -0400

    feat: add Firebase authentication setup and a login component for user access.

---
+++ src/Firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAPZp-S7f0sC__4-8VgdlJ9Xrpk75X-7aw",
+    authDomain: "directorio-obispado.firebaseapp.com",
+    projectId: "directorio-obispado",
+    storageBucket: "directorio-obispado.firebasestorage.app",
+    messagingSenderId: "1031416820681",
+    appId: "1:1031416820681:web:8882902f8c9aa8fa736e18"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
